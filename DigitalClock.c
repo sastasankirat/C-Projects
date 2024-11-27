8ffd86ae76089f1e494009651634a843738cafc5
Sourabh Singh Rawat <psychophantom001@gmail.com>
2024-11-27 18:16:11 +0530

    added system uptime display feature

--- DigitalClock.c
+++ DigitalClock.c
@@ -4,14 +4,14 @@
 #include <unistd.h>
 
 void clear_screen() {
-  #ifdef _WIN32
+#ifdef _WIN32
     system("cls");
-  #else
+#else
     system("clear");
-  #endif  
+#endif
 }
 
-int main(){
+int main() {
     int format;
     printf("Choose the time format: \n");
     printf("1. 24 Hour format\n");
@@ -19,28 +19,41 @@ int main(){
     printf("Make a choice(1/2): ");
     scanf("%d", &format);
     clear_screen();
-    time_t raw_time;
-    struct tm*current_time;
-    char buffer[50];
 
-    if(format!=1&&format!=2){format = 2;}
-    while (1) {
-    time(&raw_time);
-    current_time = localtime(&raw_time);
-    if(format == 1){
-        strftime(buffer,sizeof(buffer),"%H:%M:%S",current_time);
-        }
-    else if(format == 2){
-        strftime(buffer,sizeof(buffer),"%I:%M:%S %p",current_time);
+    if (format != 1 && format != 2) {
+        format = 2;
     }
 
-    printf("Current time: %s\n", buffer);
+    time_t raw_time, program_start_time;
+    struct tm *current_time;
+    char buffer[50];
 
-    strftime(buffer,sizeof(buffer),"%A : %B,%d,%Y",current_time);
-    printf("Current date: %s\n", buffer);
-    sleep(1);
-    clear_screen();
+    time(&program_start_time);
+
+    while (1) {
+        time(&raw_time);
+        current_time = localtime(&raw_time);
+
+        if (format == 1) {
+            strftime(buffer, sizeof(buffer), "%H:%M:%S", current_time);
+        } else if (format == 2) {
+            strftime(buffer, sizeof(buffer), "%I:%M:%S %p", current_time);
+        }
+        printf("Current time: %s\n", buffer);
+
+        strftime(buffer, sizeof(buffer), "%A : %B %d, %Y", current_time);
+        printf("Current date: %s\n", buffer);
+
+        time_t elapsed_time = raw_time - program_start_time;
+        int hours = elapsed_time / 3600;
+        int minutes = (elapsed_time % 3600) / 60;
+        int seconds = elapsed_time % 60;
+        printf("System Uptime: %02d:%02d:%02d\n", hours, minutes, seconds);
+
+        sleep(1);
+        clear_screen();
     }
 
     return 0;
 }
+

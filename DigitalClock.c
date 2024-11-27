38e84fb791ad6ccf8a938330268569fca408374d
Sourabh Singh Rawat <psychophantom001@gmail.com>
2024-11-27 18:44:10 +0530

    Added text colour feature : yellow for morning , green for afternoon , blue for rest

--- DigitalClock.c
+++ DigitalClock.c
@@ -11,6 +11,20 @@ void clear_screen() {
 #endif
 }
 
+void set_text_color(int hour) {
+    if (hour >= 6 && hour < 12) {
+        printf("\033[1;33m");
+    } else if (hour >= 12 && hour < 18) {
+        printf("\033[1;32m");
+    } else {
+        printf("\033[1;34m");
+    }
+}
+
+void reset_text_color() {
+    printf("\033[0m");
+}
+
 int main() {
     int format;
     printf("Choose the time format: \n");
@@ -34,6 +48,8 @@ int main() {
         time(&raw_time);
         current_time = localtime(&raw_time);
 
+        set_text_color(current_time->tm_hour);
+
         if (format == 1) {
             strftime(buffer, sizeof(buffer), "%H:%M:%S", current_time);
         } else if (format == 2) {
@@ -49,6 +65,8 @@ int main() {
         int minutes = (elapsed_time % 3600) / 60;
         int seconds = elapsed_time % 60;
         printf("System Uptime: %02d:%02d:%02d\n", hours, minutes, seconds);
+
+        reset_text_color();
 
         sleep(1);
         clear_screen();
